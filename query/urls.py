d8f6e2105081811996279131f844a3e75526a7ec
FDS-Andrew <98504576+FDS-Andrew@users.noreply.github.com>
2022-08-11 18:04:43 +0800

    Update urls.py

--- query/urls.py
+++ query/urls.py
@@ -1,12 +1,9 @@
 from django.urls import path
 from . import views
-from django.contrib.staticfiles.storage import staticfiles_storage
-from django.views.generic.base import RedirectView
 
 urlpatterns = [
     path('', views.index, name='index'),
     path('search/', views.search, name='search'),
     path('search/whoisdetails/', views.whoisdetails, name='whoisdetails'),
     path('search/nsdetails/', views.nsdetails, name='nsdetails'),
-    path('favicon.ico', RedirectView.as_view(url=staticfiles_storage.url('img/favicon.ico'))),
 ]
