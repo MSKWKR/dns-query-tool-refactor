fc56484bb1c5ecf42433348fadc898f4e1638527
Andrew Wang <andrewwang@freedom.net.tw>
2022-08-24 06:17:59 +0000

    Deleted testinggorund.py

--- testing/testinggorund.py
+++
@@ -1,9 +0,0 @@
-from ipwhois.net import Net
-from ipwhois.asn import IPASN
-try:
-    net = Net("122.146.12.30")
-    obj = IPASN(net)
-    results = obj.lookup()
-    print(" ASN:", results['asn'], '|', +"Country:", results['asn_country_code'], '|', "ASN registry:", results['asn_registry'].upper(), '|', "Description:", results['asn_description'])
-except Exception:
-    print("\nNo ASN records")
