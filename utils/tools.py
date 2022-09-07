e2181d75f2c6f872aedab14ab8d89be1b2da1c85
edwardlin <edwardlin@freedom.net.tw>
2022-09-07 18:07:26 +0800

    Finish logic for A and AAAA, should merge the methods to a generic method that takes multiple differnt inputs.

--- utils/tools.py
+++ utils/tools.py
@@ -10,46 +10,51 @@ class DNSToolBox:
         # set implicit domain string
         self._domain_string = None
 
-    # initialize the tool domain string to the given string
-    def set_domain_string(self, domain_string: str):
+    # initialize and return domain string to the class instance
+    def set_domain_string(self, domain_string: str) -> str:
         self._domain_string = domain_string
+        return domain_string
 
     # check factor A for IPv4
     @property
     def A(self) -> str:
         # the answer is an iterator of 'A' records, need to loop through in order to get the data
         answers = dns.resolver.resolve(self._domain_string, "A")
-        for rdata in answers:
-            print("Host", rdata.exchange, "has preference", rdata.preference)
+        for answer in answers:
+            # answer is a DNS object, return a converted string
+            return str(answer)
 
     # check factor AAAA for IPv6
     @property
     def AAAA(self) -> str:
-        return "aaaa"
+        answers = dns.resolver.resolve(self._domain_string, "AAAA")
+        for answer in answers:
+            # answer is a DNS object, return a converted string
+            return str(answer)
 
     # check factor NS
-    @property
-    def NS(self):
-        return "NS"
+    # @property
+    # def NS(self):
+    #     return "NS"
 
     # check factor MX
-    @property
-    def MX(self):
-        answers = dns.resolver.resolve(self._domain_string, "MX")
-        for rdata in answers:
-            print("Host", rdata.exchange, "has preference", rdata.preference)
+    # @property
+    # def MX(self):
+    #     answers = dns.resolver.resolve(self._domain_string, "MX")
+    #     for rdata in answers:
+    #         print("Host", rdata.exchange, "has preference", rdata.preference)
 
     # check whether the given site has a www domain
-    @property
-    def have_www(self) -> bool:
-        return True
+    # @property
+    # def have_www(self) -> bool:
+    #     return True
 
 
 def main():
     toolbox = DNSToolBox()
-    test_site = "freedom.net.tw"
+    test_site = "example.com"
     toolbox.set_domain_string(test_site)
-    print(toolbox.MX)
+    print(toolbox.AAAA)
 
 
 if __name__ == "__main__":
