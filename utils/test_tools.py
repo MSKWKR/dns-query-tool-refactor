4c628f55feeea6cad54f597ce3ae900e30c59c5b
edwardlin <edwardlin@freedom.net.tw>
2022-09-08 16:25:06 +0800

    Finish the test for generic tool get_result

--- utils/test_tools.py
+++ utils/test_tools.py
@@ -1,3 +1,4 @@
+import dns
 import pytest
 from utils import tools
 
@@ -8,19 +9,39 @@ class TestDNSToolBox:
     # is this the valid way?
     TEST_TOOLBOX.set_domain_string(TEST_DOMAIN)
 
-    @pytest.mark.parametrize("test_input, expected",
-                             [(TEST_DOMAIN, TEST_DOMAIN)])
+    @pytest.mark.parametrize("test_input, expected", [
+        pytest.param(TEST_DOMAIN, TEST_DOMAIN),
+        pytest.param("", ""),
+        pytest.param("not a domain", "not a domain")
+    ])
     def test_set_domain_string(self, test_input, expected):
         # the set domain string equal to the given domain constant
         assert self.TEST_TOOLBOX.set_domain_string(test_input) == expected
 
-    def test_A(self):
+    # ----------------------- domain string test -------------------------------
 
-        want = "93.184.216.34"
-        have = self.TEST_TOOLBOX.A
-        assert want == have
+    @pytest.mark.xfail(raises=dns.resolver.NoNameservers)
+    def test_get_result_empty_domain(self):
+        # test empty input
+        self.TEST_TOOLBOX.set_domain_string("")
+        self.TEST_TOOLBOX.get_result("A")
 
-    def test_AAAA(self):
-        want = "2606:2800:220:1:248:1893:25c8:1946"
-        have = self.TEST_TOOLBOX.AAAA
-        assert want == have
+    # should xpass since it should raise and error
+    @pytest.mark.xfail(raises=dns.resolver.NoNameservers)
+    # not sure if the error raised here is correct?
+    def test_get_result_wrong_domain_string(self):
+        # test wrong input
+        self.TEST_TOOLBOX.set_domain_string("sdfasdfsdfsdf")
+        self.TEST_TOOLBOX.get_result("A")
+
+    # ----------------------- different input type test ------------------------------
+    @pytest.mark.parametrize("test_input, expected", [
+        pytest.param("a", "93.184.216.34"),
+        pytest.param("aaaa", "2606:2800:220:1:248:1893:25c8:1946"),
+        pytest.param("www", "www.example.com"),
+        pytest.param("mx", "0 ."),
+        pytest.param("soa", "ns.icann.org. noc.dns.icann.org. 2022090401 7200 3600 1209600 3600")
+    ])
+    def test_get_result(self, test_input, expected):
+        self.TEST_TOOLBOX.set_domain_string(self.TEST_DOMAIN)
+        assert self.TEST_TOOLBOX.get_result(test_input) == expected
