769550eb5dd5bdd62448c2459b24b2e7bede26f4
edwardlin <edwardlin@freedom.net.tw>
2022-09-08 11:52:49 +0800

    Refactor single search tool to a migrated generic type search. Done with A, AAAA, MX, NS, TXT, SOA and WWW.

--- utils/tools.py
+++ utils/tools.py
@@ -3,69 +3,70 @@ import dns.resolver
 import dns.exception
 
 
-# DNS_EXCEPTIONS = (dns.exception.FormError, dns.exception.SyntaxError, dns.exception.UnexpectedEnd,
-#                       dns.exception.TooBig, dns.exception.Timeout, dns.resolver.NXDOMAIN, dns.resolver.NoAnswer,
-#                       dns.resolver.NoNameservers)
-
-
-# DNSToolBox is a generic-style set that contains the method that needed to check the domain string
 class DNSToolBox:
+    """
+    DNSToolBox is a generic-style set that contains the method that needed to check the domain string
+    """
+
     # the tool is defaulted with nothing
     def __init__(self):
-        # set implicit domain string
+        """Initializes class attribute"""
         self._domain_string = None
 
-    # initialize and return domain string to the class instance
     def set_domain_string(self, domain_string: str) -> str:
+        """Set_domain_string parses, initialize and return the pure domain string
+        :param domain_string: str
+        :return: str
+        """
+
         self._domain_string = domain_string
         return domain_string
 
-    # check factor A for IPv4
-    @property
-    def A(self) -> str:
-        try:
-            # the answer is an iterator of 'A' records, need to loop through in order to get the data
-            answers = dns.resolver.resolve(self._domain_string, "A")
-            for answer in answers:
-                # answer is a dns.resolver.Answer instance, return type should be a converted string
-                return str(answer)
+    def search_with_type(self, record_type: str) -> str:
+        """
+        The method search with type is a generic function that takes the given record type and search with dns resolver.
 
-        # Error handling when input domain is incorrect
-        except Exception as error:
-            match Exception:
-                # Handle expected error
-                case (dns.exception.FormError, dns.exception.SyntaxError, dns.exception.UnexpectedEnd,
-                      dns.exception.TooBig, dns.exception.Timeout, dns.resolver.NXDOMAIN, dns.resolver.NoAnswer,
-                      dns.resolver.NoNameservers):
-                    return f"Domain Name Error as: {error}"
-                # Handle unexpected error
-                case _:
-                    return f"Unexpected Error as: {error}"
+        Can accept input record type 'A', 'AAAA', 'NS', 'MX', 'TXT', 'SOA', 'WWW' with its upper and lower cases.
+        Return an error string if input record type is incorrect or the domain itself malfunctioned.
 
-    # check factor AAAA for IPv6
-    @property
-    def AAAA(self) -> str:
-        answers = dns.resolver.resolve(self._domain_string, "AAAA")
-        for answer in answers:
-            # answer is a DNS object, return a converted string
-            return str(answer)
+        :param record_type: DNS record type
+        :type record_type: str
 
-    # check factor NS
-    # @property
-    # def NS(self):
-    #     return "NS"
+        :return: The search result
+        :rtype: str
+        """
+        # print(f"{record_type:}")
+        # turn input all to uppercase for comparison
+        record_type = record_type.upper()
+        match record_type:
+            case ("A" | "AAAA" | "NS" | "MX" | "TXT" | "SOA"):
+                try:
+                    # the answer is an iterator of type records, need to loop through in order to get the data
+                    answers = dns.resolver.resolve(self._domain_string, record_type)
+                    for answer in answers:
+                        # answer is a dns.resolver.Answer instance, return type should be a converted string
+                        return str(answer)
 
-    # check factor MX
-    # @property
-    # def MX(self):
-    #     answers = dns.resolver.resolve(self._domain_string, "MX")
-    #     for rdata in answers:
-    #         print("Host", rdata.exchange, "has preference", rdata.preference)
-
-    # check whether the given site has a www domain
-    # @property
-    # def have_www(self) -> bool:
-    #     return True
+                # Error handling when input domain is incorrect
+                except Exception as error:
+                    match Exception:
+                        # Handle expected error
+                        case (dns.exception.FormError | dns.exception.SyntaxError | dns.exception.UnexpectedEnd,
+                              dns.exception.TooBig | dns.exception.Timeout | dns.resolver.NXDOMAIN
+                              | dns.resolver.NoAnswer | dns.resolver.NoNameservers):
+                            return f"Domain Name Error: {error}"
+                        # Handle unexpected error
+                        case _:
+                            return f"Unexpected Error: {error}"
+            case "WWW":
+                try:
+                    answers = dns.resolver.resolve("www." + self._domain_string, "A")
+                    if answers:
+                        return f"www.{self._domain_string}"
+                except Exception as error:
+                    return f"www domain error: {error}"
+            case _:
+                raise NameError(f"Record Type Input Error: {record_type}")
 
 
 def main():
@@ -74,7 +75,8 @@ def main():
     while True:
         test_site = input("Enter Domain Name: ")
         toolbox.set_domain_string(test_site)
-        print("\n", toolbox.A, "\n")
+        search_type = input("Enter Records you want to check: ").upper()
+        print("\n", toolbox.search_with_type(search_type), "\n")
         if input("Do you want to continue? (y/n)").lower() == "n":
             break
 
