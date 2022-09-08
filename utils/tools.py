4ed03e20fdea6f27ecc1f8e0c05cef3d0d11816c
edwardlin <edwardlin@freedom.net.tw>
2022-09-08 16:27:03 +0800

    Refactor and add get_result, search_www and search

--- utils/tools.py
+++ utils/tools.py
@@ -1,6 +1,8 @@
 """DNS Tool Box"""
 import dns.resolver
 import dns.exception
+import socket
+from typing import Union, List
 
 
 class DNSToolBox:
@@ -12,59 +14,100 @@ class DNSToolBox:
     def __init__(self):
         """Initializes class attribute"""
         self._domain_string = None
+        self._res = dns.resolver.Resolver()
 
     def set_domain_string(self, domain_string: str) -> str:
         """Set_domain_string parses, initialize and return the pure domain string
-        :param domain_string: str
-        :return: str
+        :param domain_string: The original input string(unparsed)
+        :type: str
+
+        :return: The parsed, usable domain
+        :rtype: str
         """
 
         self._domain_string = domain_string
         return domain_string
 
-    def search_with_type(self, record_type: str) -> str:
+    def search(self, record_type: str) -> Union[dns.resolver.Resolver, None]:
         """
-        The method search with type is a generic function that takes the given record type and search with dns resolver.
+        The method search with type is a generic function
+        wraps search of the given record type and teh exceptions.
+        :param record_type:
+        :return:
+        """
+        try:
+            return self._res.resolve(self._domain_string, record_type)
+        # Return None when input domain is incorrect
+        except (dns.exception.FormError, dns.exception.SyntaxError, dns.exception.UnexpectedEnd,
+                dns.exception.TooBig, dns.exception.Timeout, dns.resolver.NXDOMAIN
+                , dns.resolver.NoAnswer, dns.resolver.NoNameservers, dns.resolver.YXDOMAIN,
+                dns.name.EmptyLabel, socket.error):
+            return None
 
-        Can accept input record type 'A', 'AAAA', 'NS', 'MX', 'TXT', 'SOA', 'WWW' with its upper and lower cases.
-        Return an error string if input record type is incorrect or the domain itself malfunctioned.
+    def search_www(self) -> Union[dns.resolver.Resolver, None]:
+        """
+        Search_www check www domain by checking it's A Record.
+        :return:
+        """
+        try:
+            answers = dns.resolver.resolve("www." + self._domain_string, "A")
+            if answers:
+                return answers
+            else:
+                return None
+
+        except (dns.exception.FormError, dns.exception.SyntaxError, dns.exception.UnexpectedEnd,
+                dns.exception.TooBig, dns.exception.Timeout, dns.resolver.NXDOMAIN
+                , dns.resolver.NoAnswer, dns.resolver.NoNameservers, dns.resolver.YXDOMAIN,
+                dns.name.EmptyLabel, socket.error):
+            return None
+
+    def get_result(self, record_type: str) -> Union[str, List]:
+        """
+        Function for getting the asked Record Type.
+        Can accept input Record type 'A', 'AAAA', 'NS', 'MX', 'TXT', 'SOA', 'WWW' with its upper and lower cases.
+        Return an empty string if input record type is incorrect or the domain itself malfunctioned.
 
         :param record_type: DNS record type
         :type record_type: str
 
-        :return: The search result
-        :rtype: str
+        :return: The transformed search result
+        :rtype: str, List
         """
         # print(f"{record_type:}")
         # turn input all to uppercase for comparison
         record_type = record_type.upper()
         match record_type:
-            case ("A" | "AAAA" | "NS" | "MX" | "TXT" | "SOA" | "CNAME"):
-                try:
-                    # the answer is an iterator of type records, need to loop through in order to get the data
-                    answers = dns.resolver.resolve(self._domain_string, record_type)
+            case ("A" | "AAAA" | "MX" | "SOA" | "CNAME"):
+                answers = self.search(record_type)
+                if answers:
+                    # answers is an iterator of type records, need to loop through in order to get the data
                     for answer in answers:
                         # answer is a dns.resolver.Answer instance, return type should be a converted string
                         return str(answer)
+                else:
+                    return ""
 
-                # Error handling when input domain is incorrect
-                except Exception as error:
-                    match Exception:
-                        # Handle expected error
-                        case (dns.exception.FormError | dns.exception.SyntaxError | dns.exception.UnexpectedEnd,
-                              dns.exception.TooBig | dns.exception.Timeout | dns.resolver.NXDOMAIN
-                              | dns.resolver.NoAnswer | dns.resolver.NoNameservers):
-                            return f"Domain Name Error: {error}"
-                        # Handle unexpected error
-                        case _:
-                            return f"Unexpected Error: {error}"
+            case "NS":
+                answers = self.search(record_type)
+                ns_result = []
+                for answer in answers:
+                    ns_result.append(str(answer))
+
+                return answer
+
+            case "TXT":
+                answers = self.search(record_type)
+                txt_result = []
+                if not answers:
+                    return txt_result
+                for answer in answers:
+                    txt_result.append(str(answer))
+                return txt_result
+
             case "WWW":
-                try:
-                    answers = dns.resolver.resolve("www." + self._domain_string, "A")
-                    if answers:
-                        return f"www.{self._domain_string}"
-                except Exception as error:
-                    return f"www domain error: {error}"
+                answers = self.search_www()
+                return f"www.{self._domain_string}" if answers else ""
 
             case _:
                 raise NameError(f"Record Type Input Error: {record_type}")
@@ -78,7 +121,7 @@ def main():
         toolbox.set_domain_string(test_site)
         finished_record_type = ["a", "aaaa", "ns", "mx", "txt", "soa", "www", "cname"]
         for dns_record_type in finished_record_type:
-            result = toolbox.search_with_type(dns_record_type)
+            result = toolbox.get_result(dns_record_type)
             print(f"{dns_record_type}: {result}")
 
         if input("Do you want to continue? (y/n)").lower() == "n":
