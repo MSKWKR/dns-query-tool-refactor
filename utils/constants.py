d2aca2c550abd8d751359c1e977b621ecb8105bc
edwardlin <edwardlin@freedom.net.tw>
2022-09-13 17:44:40 +0800

    Create contants.py. Add Email Table.

---
+++ utils/constants.py
@@ -0,0 +1,15 @@
+EMAIL_TABLE = {
+    # domain
+    "aspmx.l.google.com": "Google_Workspace",
+    "smtp.google.com": "Gmail",
+    "gmail-smtp-in.l.google.com": "Gmail",
+    "outlook.com": "Office_365",
+    "hinet.net": "HiNet_Mail",
+    "amazon.com": "Amazon_SES",
+    "yahoodns.net": "Yahoo!_Mail",
+    "mailcloud.com": "Mailcloud",
+    "Mimecast": "mimecast.com",
+    "Messagelabs": "messagelabs.com",
+    "ProofPoint": "pphosted.com",
+    "中國移動": "chinamobile.com"
+}
