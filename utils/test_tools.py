da8723c99b479cb6eee311aa702bfeba12914a52
edwardlin <edwardlin@freedom.net.tw>
2022-09-21 11:02:47 +0800

    Add tests for code inworking, undone

--- utils/test_tools.py
+++ utils/test_tools.py
@@ -1,9 +1,6 @@
-import random
-
 import pytest
 
-from constants_for_test import raw_domain_check_data, correct_string_results, correct_list_results, \
-    different_incorrect_a_record
+from constants_for_test import raw_domain_check_data, correct_string_results, correct_list_results
 from tools import DNSToolBox
 
 # @pytest.fixture
@@ -47,22 +44,3 @@ def test_has_https(domain):
     have = newToolBox.has_https()
     expect = True
     assert have == expect
-
-
-# ----------------- Mock and try to break the code -------------------------------
-
-
-class MockToolBox(DNSToolBox):
-    def get_result(self, record_type: str) -> str:
-        record_type = record_type.upper()
-        match record_type:
-            case "A":
-                result = random.choice(different_incorrect_a_record)
-                if result in ["0.0.0.0", "255.255.255.255", "127.0.0.1"]:
-                    raise TypeError("Incorrect form Record A, reserved IPv4 address")
-                return result
-
-
-def test_get_result_a():
-    m = MockToolBox()
-    assert m.get_result("a") == "0.0.0.0"
