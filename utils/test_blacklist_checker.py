063587e89647419af59856229160b1786e3d3b43
edwardlin <edwardlin@freedom.net.tw>
2022-09-21 14:39:28 +0800

    Change variable name

--- utils/test_blacklist_checker.py
+++ utils/test_blacklist_checker.py
@@ -1,12 +1,12 @@
 import pytest
 
 from blacklist_checker import BlackListChecker
-from constants_for_test import blacklisted_site
+from constants_for_test import blacklisted_sites
 
 test_black_list_checker = BlackListChecker()
 
 
-@pytest.mark.parametrize("input_", blacklisted_site)
+@pytest.mark.parametrize("input_", blacklisted_sites)
 def test_is_black_listed(input_):
     have = test_black_list_checker.is_black_listed(input_)
     expected = True
