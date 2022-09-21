62196b4df1312b074380c94ba6f73fd5d0e1671c
edwardlin <edwardlin@freedom.net.tw>
2022-09-21 14:38:16 +0800

    Add test for invalid AAAA record

--- utils/test_valid_result.py
+++ utils/test_valid_result.py
@@ -1,14 +1,21 @@
 import pytest
 
-from constants_for_test import different_incorrect_a_record
+from constants_for_test import different_incorrect_a_records, different_incorrect_aaaa_records
 from valid_result import Validator
 
 test_validator = Validator()
 
 
-@pytest.mark.parametrize("input_", different_incorrect_a_record)
+@pytest.mark.parametrize("input_", different_incorrect_a_records)
 def test_is_invalid_a(input_: str):
     have = test_validator.is_valid("A", input_)
     # the test checks all the incorrect a record
+    expected = False
+    assert have == expected
+
+
+@pytest.mark.parametrize("input_", different_incorrect_aaaa_records)
+def test_is_invalid_aaaa(input_):
+    have = test_validator.is_valid("AAAA", input_)
     expected = False
     assert have == expected
