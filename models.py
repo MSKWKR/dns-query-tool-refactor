044d9095313ab6c0bd7a9c4d5e414d9a5fc82a76
edwardlin <edwardlin@freedom.net.tw>
2022-09-23 16:39:55 +0800

    Add the correct fields in-use for SQLmodel

--- models.py
+++ models.py
@@ -1,33 +1,38 @@
-import datetime
 from typing import Optional, List
 
 from sqlmodel import Field, SQLModel
 
 
-class Domain(SQLModel, table=True):
+class Domain(SQLModel):
+    """
+    Model class for a specific domain
+    """
     id: Optional[int] = Field(default=None, primary_key=True)
     domain_string: str = Field(index=True)
-    # shouldn't be List, change later
-    dns_record: List['DNSRecord']
 
 
-class DNSRecord(SQLModel, table=True):
-    check_time: datetime.datetime
+class DNSRecord(SQLModel):
+    """
+    Model class for a specific domain search
+    """
+    check_time: str
     a: str
     aaaa: str
     mx: str
     soa: str
     www: str
-    ns: str
-    txt: str
-    ipv4: str
-    ipv6: str
-    asn: str
-    xfr: str
+    ns: List[str]
+    txt: List[str]
+    ipv4: List[str]
+    ipv6: List[str]
+    asn: dict[str:List[str]]
+    xfr: List[str]
     ptr: str
     registrar: str
     expiration_date: str
-    srv: str
+    srv: dict[str: List[str]]
     email_exchange_service: str
-    has_https: str
-    is_blacklisted: str
+    o365: dict[str: List[str]]
+    has_https: bool
+    is_blacklisted: bool
+    domain_id: Optional[int] = Field(default=None, foreign_key="Domain.id")
