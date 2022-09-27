fbbe15ae07b092e5b50a7ee5f3692a83568f937d
edwardlin <edwardlin@freedom.net.tw>
2022-09-27 17:13:04 +0800

    Add new tests in test_valid_results

--- src/utils/test_valid_result.py
+++ src/utils/test_valid_result.py
@@ -1,9 +1,10 @@
 import pytest
 
-from src.utils.constants_for_test import different_incorrect_a_records, different_incorrect_aaaa_records
+from src.utils.constants_for_test import different_incorrect_a_records, different_incorrect_aaaa_records, \
+    different_incorrect_mx_records, different_incorrect_soa_records, different_incorrect_srv_records
 from src.utils.valid_result import Validator
 
-test_validator = Validator()
+test_validator = Validator("example.com")
 
 
 @pytest.mark.parametrize("input_", different_incorrect_a_records)
@@ -15,7 +16,31 @@ def test_is_invalid_a(input_: str):
 
 
 @pytest.mark.parametrize("input_", different_incorrect_aaaa_records)
-def test_is_invalid_aaaa(input_):
+def test_is_invalid_aaaa(input_: str):
     have = test_validator.is_valid("AAAA", input_)
     expected = False
+    assert have == expected
+
+
+@pytest.mark.parametrize("input_", different_incorrect_mx_records)
+def test_invalid_mx(input_: str):
+    have = test_validator.is_valid("MX", input_)
+    expected = False
+
+    assert have == expected
+
+
+@pytest.mark.parametrize("input_", different_incorrect_soa_records)
+def test_invalid_soa(input_: str):
+    have = test_validator.is_valid("SOA", input_)
+    expected = False
+
+    assert have == expected
+
+
+@pytest.mark.parametrize("input_", different_incorrect_srv_records)
+def test_is_invalid_srv(input_):
+    have = test_validator.is_valid_srv(input_)
+    expected = False
+
     assert have == expected
