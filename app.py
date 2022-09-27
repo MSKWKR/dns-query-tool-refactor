d05a9c667087ea63f390f7800a33d78e9f9be0a0
edwardlin <edwardlin@freedom.net.tw>
2022-09-27 11:25:45 +0800

    Update app.py for tryouts

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from pprint import pprint
 
-from src.model.models import DNSRecord
+from sqlmodel import create_engine, SQLModel, Session
+
+from src.model.models import to_domain, to_DNS_record
 from src.utils.tools import DNSToolBox
 
 
@@ -13,36 +15,23 @@ def main():
     # domain_database_engine = database.instantiate_engine(sqlite_url)
 
     while continue_:
-        test_site = input("Enter Domain Name: ")
-        toolbox.set_domain_string(test_site)
-        domain_search_result = toolbox.domain_info
-        pprint(domain_search_result)
+        domain_to_check = input("Enter Domain Name: ")
+        toolbox.set_domain_string(domain_to_check)
+        print("Fetching Records...")
+        domain_info = to_domain(domain_to_check)
+        a = toolbox.domain_info
+        pprint(a)
+        dns_record = to_DNS_record(a)
 
-        record = DNSRecord(
-            check_time=domain_search_result["check_time"],
-            a=domain_search_result["a"],
-            aaaa=domain_search_result["aaaa"],
-            mx=domain_search_result["mx"],
-            soa=domain_search_result["soa"],
-            www=domain_search_result["www"],
-            ns=domain_search_result["ns"],
-            txt=domain_search_result["txt"],
-            ipv4=domain_search_result["ipv4"],
-            ipv6=domain_search_result["ipv6"],
-            asn=domain_search_result["asn"],
-            xfr=domain_search_result["xfr"],
-            ptr=domain_search_result["ptr"],
-            registrar=domain_search_result["registrar"],
-            expiration_date=domain_search_result["expiration_date"],
-            srv=domain_search_result["srv"],
-            email_exchange_service=domain_search_result["email_exchange_service"],
-            o365=domain_search_result["o365"],
-            has_https=domain_search_result["has_https"],
-            is_blacklisted=domain_search_result["is_blacklisted"]
-        )
-        print(record)
-        print(record.www)
-        print(record.check_time)
+        print("Records Fetched")
+
+        engine = create_engine("sqlite:///database.db")
+        SQLModel.metadata.create_all(engine)
+
+        with Session(engine) as session:
+            session.add(domain_info)
+            # session.add(dns_record)
+            session.commit()
 
         continue_ = False
 
