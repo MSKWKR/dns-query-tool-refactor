db3efc6f0939a9928f2dc5b7ff3ab20664de9455
edwardlin <edwardlin@freedom.net.tw>
2022-09-28 16:00:54 +0800

    Update testouts

--- app.py
+++ app.py
@@ -18,18 +18,24 @@ def main():
         print("Fetching Records...")
 
         domain = models.to_domain(domain_to_check)
+
         search_result = toolbox.domain_info
         domain_search_result = utils.dictionary_value_to_bytes(search_result=search_result)
         dns_record = models.to_DNS_record(domain_search_result)
         print("Records Fetched.\n")
 
-        print(f"{dns_record=}")
         engine = database.instantiate_engine(db_url=sqlite_url)
         if not database_exists(url=sqlite_url):
             database.create_database_and_tables(engine)
 
-        database.add_data(db_engine=engine, data=domain)
-        database.add_data(db_engine=engine, data=dns_record)
+        # Do nothing if domain exists
+        database.add_domain_data(db_engine=engine, data=domain)
+        # Will still add the domain search record
+        database.add_domain_record_data(db_engine=engine, data=dns_record)
+
+        a = database.read_data_from_domain_name(engine, "google.com")
+        print(a.domain_name, a.search_used_time)
+        print(utils.bytes_decrypt(a.a))
 
         if input("Do you want to continue? (y/n)").lower() == "n":
             continue_ = False
