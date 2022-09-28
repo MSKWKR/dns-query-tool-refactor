9c78dad9b73058b5c9e70f6e9e930f0963f1b320
edwardlin <edwardlin@freedom.net.tw>
2022-09-28 12:22:19 +0800

    Add decrypt function

--- src/model/utils.py
+++ src/model/utils.py
@@ -1,19 +1,32 @@
 import pickle
 
 
-def dictionary_value_to_bytes(search_results: dict) -> dict:
+def dictionary_value_to_bytes(search_result: dict) -> dict:
     """
     Helper function for taking the searched result and pickle all field values except for search_used_time and check_time
-    :param search_results: The DNS search result
+    :param search_result: The DNS search result
     :type: dict
 
     :return: The altered dictionary
     :rtype: dict
     """
-    for record_key in search_results:
+    for record_key in search_result:
         if record_key in ["search_used_time", "check_time"]:
             continue
 
-        search_results[record_key] = pickle.dumps(search_results[record_key])
+        search_result[record_key] = pickle.dumps(search_result[record_key])
 
-    return search_results
+    return search_result
+
+
+def bytes_decrypt(bytes_form_data: bytes) -> any:
+    """
+    Helper function to decrypt the given data
+
+    :param bytes_form_data: Data to decrypt
+    :type: bytes
+
+    :return: The original form of the data
+    :rtype: any
+    """
+    return pickle.loads(bytes_form_data)
