89a7562e79da93a42b04ddc504c1035995b03bcc
edwardlin <edwardlin@freedom.net.tw>
2022-09-29 10:04:45 +0800

    Update app workflow

--- app.py
+++ app.py
@@ -9,36 +9,38 @@ sqlite_url = f"sqlite:///{sqlite_file_name}"
 
 def main():
     toolbox = DNSToolBox()
-    continue_ = True
+    domain_database = database.DomainDatabase()
+    domain_database.set_db_url(url=sqlite_url)
+    domain_database.db_engine = domain_database.instantiate_engine()
 
-    while continue_:
-        domain_to_check = input("Enter Domain Name: ")
+    test_domains = [
+        # "example.com", "freedom.net.tw", "google.com", "python.org", "freedom.net.tw", "google.com", "python.org"
+        "google.com",
+    ]
+    for domain in test_domains:
 
+        domain_to_check = domain
         toolbox.set_domain_string(domain_to_check)
-        print("Fetching Records...")
 
+        print(f"Fetching Records: {domain}")
+        # Add to domain table
         domain = models.to_domain(domain_to_check)
-
         search_result = toolbox.domain_info
+        # Data serializing
         domain_search_result = utils.dictionary_value_to_bytes(search_result=search_result)
+        # Add to dns record table
         dns_record = models.to_DNS_record(domain_search_result)
         print("Records Fetched.\n")
 
-        engine = database.instantiate_engine(db_url=sqlite_url)
         if not database_exists(url=sqlite_url):
-            database.create_database_and_tables(engine)
+            domain_database.create_database_and_tables()
 
         # Do nothing if domain exists
-        database.add_domain_data(db_engine=engine, data=domain)
+        domain_database.add_domain_data(domain_data=domain)
         # Will still add the domain search record
-        database.add_domain_record_data(db_engine=engine, data=dns_record)
+        domain_database.add_domain_record_data(domain_record_data=dns_record)
 
-        a = database.read_data_from_domain_name(engine, "google.com")
-        print(a.domain_name, a.search_used_time)
-        print(utils.bytes_decrypt(a.a))
-
-        if input("Do you want to continue? (y/n)").lower() == "n":
-            continue_ = False
+        # print(f"Used time: {domain_database.read_data_from_domain_name(domain).search_used_time}")
 
 
 if __name__ == "__main__":
