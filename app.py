ce7c44d9444bae5f352784f33505ed0e804c50d8
edwardlin <edwardlin@freedom.net.tw>
2022-10-05 11:52:57 +0800

    Alter app workflow

--- app.py
+++ app.py
@@ -1,17 +1,11 @@
 from pprint import pprint
 
-from src import fetcher
-from src.model.utils import result_decrypt
-
-sqlite_file_name = "domain_record.db"
-sqlite_url = f"sqlite:///{sqlite_file_name}"
+from src.fetcher import get_records
 
 
 def main():
     test_d = "google.com"
-    test_fetcher = fetcher.DNSRecordFetcher(test_d, sqlite_url)
-    a = test_fetcher.get_records()
-    result_decrypt(a)
+    a = get_records(test_d)
     pprint(a)
 
 
