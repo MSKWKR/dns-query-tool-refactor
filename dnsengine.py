2071bb139d59824c7eb507618b992e6a1296c786
edwardlin <edwardlin@freedom.net.tw>
2022-10-12 16:06:30 +0800

    Change the CLI logic

--- dnsengine.py
+++ dnsengine.py
@@ -10,15 +10,12 @@ from src.fetcher import get_records, get_record, get_record_to_json
 @click.option('-t', '--rtype', type=str, help='DNS record type to search')
 @click.option('-b', '--blob', type=str, help='Output to json file')
 def app(domain: str, rtype: Optional[str], blob: Optional[str]):
-    if rtype:
-        # if type provided, return only the specific type
-        answer = get_record(domain_string=domain, record_type=rtype)
-        click.echo(answer)
-    elif blob:
+    if blob:
         get_record_to_json(domain_string=domain, file_name=blob)
-
     else:
-        answer = get_records(domain_string=domain)
+        # if type provided, return only the specific type
+        answer = get_record(domain_string=domain, record_type=rtype) if rtype else get_records(domain_string=domain)
+        get_record_to_json(domain_string=domain, file_name="dnsrecord")
         click.echo(answer)
 
 
