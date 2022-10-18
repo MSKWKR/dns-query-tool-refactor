8c7529d86bf2f35091b925574f0b385414b63959
edwardlin <edwardlin@freedom.net.tw>
2022-10-18 16:43:59 +0800

    Remove option output to json

--- dnsengine.py
+++ dnsengine.py
@@ -8,15 +8,11 @@ from src.fetcher import get_records, get_record, get_record_to_json
 @click.command()
 @click.argument('domain')
 @click.option('-t', '--rtype', type=str, help='DNS record type to search')
-@click.option('-b', '--blob', type=str, help='Output to json file')
-def app(domain: str, rtype: Optional[str], blob: Optional[str]):
-    if blob:
-        get_record_to_json(domain_string=domain, file_name=blob)
-    else:
-        # if type provided, return only the specific type
-        answer = get_record(domain_string=domain, record_type=rtype) if rtype else get_records(domain_string=domain)
-        get_record_to_json(domain_string=domain, file_name="dnsrecord")
-        click.echo(answer)
+def app(domain: str, rtype: Optional[str]):
+    # if type provided, return only the specific type
+    answer = get_record(domain_string=domain, record_type=rtype) if rtype else get_records(domain_string=domain)
+    get_record_to_json(domain_string=domain, file_name="dnsrecord")
+    click.echo(answer)
 
 
 if __name__ == "__main__":
