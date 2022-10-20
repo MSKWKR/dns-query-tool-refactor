b2b7801848a8a07a3000b317958973df29676661
edwardlin <edwardlin@freedom.net.tw>
2022-10-20 12:03:20 +0800

    Create an API that serves the search result

---
+++ api_engine.py
@@ -0,0 +1,27 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+from src.fetcher import get_records
+
+app = FastAPI()
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+@app.get("/")
+def home():
+    return {"message": "welcome to dnsengine"}
+
+
+@app.get("/results/{domain_name}")
+def search_dns_record(domain_name: str):
+    result = get_records(domain_string=domain_name)
+    # FastAPI automatically returns Content-Type: Application/json
+    return result
